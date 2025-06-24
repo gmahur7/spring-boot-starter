bd6ebd1a63d679b7c5634275961fc0d24b7849bb
gmahur7 <gm7017308@gmail.com>
2025-06-24 08:28:34 +0530

    add: basic security using spring security v6.4

---
+++ src/main/java/com/gaurav/restApi/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.gaurav.restApi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/users/**").hasRole("USER")
+                .requestMatchers("/journals/**").hasRole("ADMIN")
+                .requestMatchers("health-check").permitAll()
+                .anyRequest().authenticated())
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        var user = User
+                .builder()
+                .username("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+
+        var admin = User
+                .builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        var manager = User
+                .builder()
+                .username("manager")
+                .password(passwordEncoder().encode("manager123"))
+                .roles("MANAGER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin, manager);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
